9f3afe525664c170765512703ce8b8370cbf3f75
covenantpathcalphark-png <covenantpath.calphark@gmail.com>
2026-02-27 10:51:27 +0530

    Update server.py

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:5501",
         "http://localhost:5500",
         "http://localhost:5501",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -227,3 +228,4 @@ def get_matches(user: dict):
     matches.sort(key=lambda x: x["match_score"], reverse=True)
 
     return {"matches": matches[:10]}
+
